86d60d42b4ec48cc06c054e3344dc66404c88f40
Jackson McHugh <jackson.mchugh@gmail.com>
2022-09-02 10:54:16 -0400

    updated team routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = Router()
 const { team, player, stat } = require('../controllers')
 
 // team routes
-
+router.get('/teams', teams.getAllTeams)
+router.post('/teams', teams.createTeam)
+router.get('/teams/:id', teams.getOneTeam)
+router.post('/teams/:id/players', players.createPlayer)
 // player routes
 
 // stat routes
