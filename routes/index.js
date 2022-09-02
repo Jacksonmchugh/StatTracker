093b213a0f0877674d205b18256af6bf57989664
Jackson McHugh <jackson.mchugh@gmail.com>
2022-09-02 11:30:05 -0400

    updated player routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const router = Router()
-const { team, player, stat } = require('../controllers')
+const { teams, players, stats } = require('../controllers')
 
 // team routes
 router.get('/teams', teams.getAllTeams)
@@ -8,5 +8,8 @@ router.post('/teams', teams.createTeam)
 router.get('/teams/:id', teams.getOneTeam)
 router.post('/teams/:id/players', players.createPlayer)
 // player routes
-
+router.get('/players', players.getAllPlayers)
+router.get('/player/:id', players.getPlayer)
+router.put('/players/:id', players.updatePlayer)
+router.delete('/players/:id', players.deletePlayer)
 // stat routes
