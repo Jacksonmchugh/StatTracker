0ab858a0f8516eb79b1a5176ca8b8a6c172d9792
Jackson McHugh <jackson.mchugh@gmail.com>
2022-09-02 10:51:22 -0400

    updating server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const db = require('./db')
+const routes = require('./routes')
+
 const PORT = process.env.PORT || 3001
 const app = express()
 
