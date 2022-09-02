98f2ea28a5233187bfe82f3faedd1ee079137e61
Jackson McHugh <jackson.mchugh@gmail.com>
2022-09-02 10:30:43 -0400

    updated players contorllers

--- Controllers/player.js
+++ Controllers/player.js
@@ -1,4 +1,5 @@
 const { Player, Team } = require('../models')
+const { rawListeners } = require('../models/team')
 
 const createPlayer = async (req, res) => {
   let team = await Team.findById(req.params.id)
@@ -8,4 +9,28 @@ const createPlayer = async (req, res) => {
   team.player.push(newPlayer._id)
   team.save()
   res.send(newPlayer)
+}
+
+const getPlayer = async (req, res) => {
+  let player = await Player.findById(req.params.id).populate('team')
+  res.send(player)
+}
+
+const updatePlayer = async (req, res) => {
+  let updatePlayer = await Player.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  })
+  res.send(updatePlayer)
+}
+
+const deletePlayer = async (req, res) => {
+  let deletePlayer = await Player.findByIdAndDelete(req.params.id)
+  res.send(deletedPlayer)
+}
+
+module.exports = {
+  getPlayer,
+  createPlayer,
+  updatePlayer,
+  deletePlayer
 }
