470b60a68708a16602f8cb7809f5be04d175c151
Jackson McHugh <jackson.mchugh@gmail.com>
2022-09-02 10:26:03 -0400

    updating teams contorllers

--- Controllers/player.js
+++ Controllers/player.js
@@ -1,1 +1,11 @@
-const { Player } = require('../models')
+const { Player, Team } = require('../models')
+
+const createPlayer = async (req, res) => {
+  let team = await Team.findById(req.params.id)
+  let playerBody = { ...req.body, team: req.params.id }
+  let newPlayer = await Player.create(playerBody)
+
+  team.player.push(newPlayer._id)
+  team.save()
+  res.send(newPlayer)
+}
