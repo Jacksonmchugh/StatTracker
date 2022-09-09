a4d40a141d76a83d0e79f21ee3099a00c6e43136
Jackson McHugh <jackson.mchugh@gmail.com>
2022-09-09 14:37:44 -0400

    nav bar styliing done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,15 +4,7 @@ import '../App.css'
 const Home = () => {
   return (
     <div className="home">
-      <div className="team_grid">
-        <h1 className="home_title">Welcome to Stat Tracker!</h1>
-
-        <img
-          className="ultimate"
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.eventimotivazionali.it%2Fwp-content%2Fuploads%2F2020%2F03%2FUltimate-Frisbee-Eventi-Motivazionali-Team-Building-Milano-5.jpg&f=1&nofb=1"
-          alt="home page"
-        />
-      </div>
+      <div className="team_grid"></div>
     </div>
   )
 }
