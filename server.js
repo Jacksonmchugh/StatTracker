0fbeb40ee0e2790319d4892146211b97d85b96cc
Jackson McHugh <jackson.mchugh@gmail.com>
2022-09-12 15:34:21 -0400

    updated db/index

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
-const db = require('./db')
+const db = require('./db/index.js')
 const { Team, Player } = require('./models')
 
 const PORT = process.env.PORT || 3001
