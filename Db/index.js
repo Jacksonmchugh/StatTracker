40719b09c43045646b6d9acc635cba3caebad21d
Jackson McHugh <jackson.mchugh@gmail.com>
2022-09-12 14:51:37 -0400

    updated db/index

--- Db/index.js
+++ Db/index.js
@@ -7,7 +7,7 @@ let dbUrl =
     : 'mongodb://127.0.0.1:27017/statTrackerDatabase'
 
 mongoose
-  .connect('process.env.MONDGODB_URL')
+  .connect('process.env.MONGODB_URL')
   .then(() => {
     console.log('Successfully connected to Stat Tracker!')
   })
